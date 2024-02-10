57814747aec7f7048f1b2907b2298a4b30a6a56d
Tohir Kuliev <kuliev.tech@gmail.com>
2024-02-10 18:50:03 -0500

    test and understand big o efficiency

---
+++ 04_isPrime.js
@@ -0,0 +1,12 @@
+const isPrime = (number) => {
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+};
+
+console.log(isPrime(11)); // true
+
+// O(N) efficiency
